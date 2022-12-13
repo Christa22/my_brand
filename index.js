4e998e0a871d41f04c37442848a1d76c8d46ae4b
Christa22 <115546518+Christa22@users.noreply.github.com>
2022-12-13 11:44:11 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var typed = new Typed('#typed', {
+    // Waits 1000ms after typing "First"
+    strings: [
+        'Web Developer', 
+        'Web Designer',
+        'Graphic Designer',
+        'UI/UX Designer',
+    ],
+
+    stringselement:null,
+    typedSpeed: 20,
+    startDelay:150,
+    backSpeed:20,
+    backDelay:1000,
+    loop: true,
+    loopCount:550,
+    showCursor:true,
+    cursorChar:"",
+    attr:null,
+    contentTypeL:"html",
+  });
