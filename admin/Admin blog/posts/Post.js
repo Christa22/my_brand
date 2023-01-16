dbf70b01dbf67063230f056ae60ebd0d87732785
Christa22 <115546518+Christa22@users.noreply.github.com>
2023-01-16 03:43:45 +0300

    Update Post.js

--- admin/Admin blog/posts/Post.js
+++ admin/Admin blog/posts/Post.js
@@ -3,29 +3,36 @@ postForm.addEventListener("submit", (event)=>{
     event.preventDefault();
 
     var inputText = document.getElementById("Title").value;
-    var inputBody = document.getElementById("Body").value;
+    var bodyDescript = document.getElementsByClassName("ck-editor__editable")[0];
     var inputTopic= document.getElementById("Topic").value;
-   
+    var picture = document.getElementById("picture");
 
-    console.log("Done", inputText,inputBody,inputTopic);
+    
+    // 'file' comes from the Blob or File API] child('bella.png')
+    const store = storage.ref().child(picture.files[0].name).put(picture.files[0]);
+    store.then((snapshot) => snapshot.ref.getDownloadURL())
+    .then(url =>{
+        console.log("url", url);
+        
+        
+            var Post = {
+              Title:inputText,
+                Body:bodyDescript.innerHTML || "",
+                Topic:inputTopic,
+                Image: url
+                }
+                console.log("done",Post);
+                db.collection("Post-Form").add(Post).then(()=>{
+                    console.log("Data has been saved")
+                   postForm.reset();
+                })
+                    
+                    .catch(()=>{
+                        console.log("Data are not Saved,Try Again!!")
+                    })
+        
+                });
 
-    var Post = {
-      Title:inputText,
-        Body:inputBody,
-        Topic:inputTopic,
-       
-        }
+    });
 
-        db.collection("Post-Form").add(Post).then(()=>{
-            console.log("Data has been saved")
-           postForm.reset();
-        })
-            
-            .catch(()=>{
-                console.log("Data are not Saved,Try Again!!")
-            })
-
-        });
-
-   
 
